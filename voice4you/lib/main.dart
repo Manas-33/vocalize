c73b9857d305f942a29da8895a8d95593fc68396
Manas Dalvi <dalvimanas33@gmail.com>
2023-01-22 06:22:04 +0530

    final2

--- voice4you/lib/main.dart
+++ voice4you/lib/main.dart
@@ -5,9 +5,66 @@ import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:firebase_ml_custom/firebase_ml_custom.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:camera/camera.dart';
 
-void main() { 
-  runApp(MyApp());
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+  runApp(const CameraApp());
+}
+
+
+class CameraApp extends StatefulWidget {
+
+  const CameraApp({Key? key}) : super(key: key);
+
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+
+            break;
+          default:
+
+            break;
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+    );
+  }
 }
 class MyApp extends StatefulWidget {
   @override
@@ -18,23 +75,23 @@ class _MyAppState extends State<MyApp> {
   File? _image;
   List<Map<dynamic, dynamic>>? _labels;
 
-  //When the model is ready, _loaded changes to trigger the screen state change.
+
   Future<String> _loaded = loadModel();
 
-  /// Triggers selection of an image and the consequent inference.
+
   Future<void> getImageLabels() async {
     try {
       final pickedFile =
           await ImagePicker().getImage(source: ImageSource.gallery);
       final image = File(pickedFile!.path);
-      // if (image == null) {
-      //   return;
-      // }
-      // TODO TFLite plugin is broken, see https://github.com/shaqian/flutter_tflite/issues/139#issuecomment-836596852
-      // var labels = List<Map>.from(await Tflite.runModelOnImage(
-      //   path: image.path,
-      //   imageStd: 127.5,
-      // ));
+      if (image == null) {
+        return;
+      }
+      TODO TFLite plugin is broken, see https://github.com/shaqian/flutter_tflite/issues/139#issuecomment-836596852
+      var labels = List<Map>.from(await Tflite.runModelOnImage(
+        path: image.path,
+        imageStd: 127.5,
+      ));
       var labels = List<Map>.from([]);
       setState(() {
         _labels = labels;
@@ -47,33 +104,28 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  /// Gets the model ready for inference on images.
+ 
   static Future<String> loadModel() async {
     final modelFile = await loadModelFromFirebase();
     return loadTFLiteModel(modelFile);
   }
 
-  /// Downloads custom model from the Firebase console and return its file.
-  /// located on the mobile device.
+
   static Future<File> loadModelFromFirebase() async {
     try {
-      // Create model with a name that is specified in the Firebase console
+
       final model = FirebaseCustomRemoteModel('action5');
 
-      // Specify conditions when the model can be downloaded.
-      // If there is no wifi access when the app is started,
-      // this app will continue loading until the conditions are satisfied.
+
       final conditions = FirebaseModelDownloadConditions(
           androidRequireWifi: true, iosAllowCellularAccess: false);
 
-      // Create model manager associated with default Firebase App instance.
       final modelManager = FirebaseModelManager.instance;
 
-      // Begin downloading and wait until the model is downloaded successfully.
+
       await modelManager.download(model, conditions);
       assert(await modelManager.isModelDownloaded(model) == true);
 
-      // Get latest model file to use it for inference by the interpreter.
       var modelFile = await modelManager.getLatestModelFile(model);
       assert(modelFile != null);
       return modelFile;
@@ -84,11 +136,9 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  /// Loads the model into some TF Lite interpreter.
-  /// In this case interpreter provided by tflite plugin.
   static Future<String> loadTFLiteModel(File modelFile) async {
     try {
-      // TODO TFLite plugin is broken, see https://github.com/shaqian/flutter_tflite/issues/139#issuecomment-836596852
+      
       final appDirectory = await getApplicationDocumentsDirectory();
       final labelsData =
           await rootBundle.load('assets/labels_mobilenet_v1_224.txt');
@@ -111,22 +161,21 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  /// Shows image selection screen only when the model is ready to be used.
+
   Widget readyScreen() {
     return MaterialApp(home:Scaffold(
       appBar: AppBar(
-        title: const Text('Firebase ML Custom example app'),
+        title: const Text('Vocalize'),
       ),
       body: Column(
         children: [
           if (_image != null)
             Image.file(_image!)
           else
-            const Text('Please select image to analyze.'),
           Column(
             children: _labels != null
                 ? _labels!.map((label) {
-                    return Text("${label["label"]}");
+                  var out=label["label"]
                   }).toList()
                 : [],
           ),
@@ -138,8 +187,6 @@ class _MyAppState extends State<MyApp> {
       ),
     ));
   }
-
-  /// In case of error shows unrecoverable error screen.
   Widget errorScreen() {
     return  MaterialApp(home: Scaffold(
       body: Center(
@@ -148,8 +195,6 @@ class _MyAppState extends State<MyApp> {
     ));
   }
 
-  /// In case of long loading shows loading screen until model is ready or
-  /// error is received.
   Widget loadingScreen() {
     return MaterialApp(home:Scaffold(
       body: Center(
@@ -166,15 +211,13 @@ class _MyAppState extends State<MyApp> {
       ),
     ));
   }
-
-  /// Shows different screens based on the state of the custom model.
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
       style: Theme.of(context).textTheme.headline2!,
       textAlign: TextAlign.center,
       child: FutureBuilder<String>(
-        future: _loaded, // a previously-obtained Future<String> or null
+        future: _loaded, 
         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
           if (snapshot.hasData) {
             return readyScreen();
@@ -186,5 +229,60 @@ class _MyAppState extends State<MyApp> {
         },
       ),
     );
+  }
+}
+
+class TextToSpeech extends StatelessWidget {
+  final FlutterTts flutterTts = FlutterTts();
+  final TextEditingController textEditingController = TextEditingController();
+  final GoogleTranslator translator = GoogleTranslator();
+
+  translate(String text,String language) async{
+    var translation = await translator
+      .translate(text, from: 'en', to: language);
+      speak(translation,language) ;
+  }
+  speak(String text,String language) async {
+    await flutterTts.setLanguage(language);
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(text);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  
+       Padding(
+            padding: const EdgeInsets.all(25),
+            child: Column(
+            
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(height: 550,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:[ElevatedButton(
+                  onPressed: () => translate(out,"eng"),
+                  child: Text("English Speech")),
+                  ElevatedButton(
+                  onPressed: () => translate(out,"hin"),
+                  child: Text("Hindi Speech")),]
+              
+              ),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children:[
+                  ElevatedButton(
+                  onPressed: () => translate(out,"tam"),
+                  child: Text("Tamil Speech")),
+                  ElevatedButton(
+                  onPressed: () => translate(out,"ml"),
+                  child: Text("Malayalam Speech")),]
+              
+              ),
+            ]),
+                       
+       )
+      
+    ;
   }
 }
